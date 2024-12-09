480a7db230a1724d892bd98398c4d5faa39eee71
흰둥이 <@192.168.45.51>
2024-12-09 23:55:31 +0900

    클래스 도입

---
+++ java_basic/src/class1/ClassStart3.java
@@ -0,0 +1,22 @@
+package class1;
+
+public class ClassStart3 {
+	
+	public static void main(String[] args) {
+		Student student1;
+		student1 = new Student();
+		student1.name = "학생1";
+		student1.age = 15;
+		student1.grade = 90;
+		
+		Student student2 = new Student();//객체, 인스턴스(둘의 차이점은 없지만 설명할때 어디소속인지 알릴때는 인스턴스라고 한다)
+		student2.name = "학생2";
+		student2.age = 16;
+		student2.grade = 80;
+		
+		System.out.println("student1 = " + student1);//패키지명.클래스명@참조값
+		System.out.println("student2 = " + student2);
+		System.out.println("이름 : " + student1.name + " 나이 : " + student1.age + " 성적 : " + student1.grade);
+		System.out.println("이름 : " + student2.name + " 나이 : " + student2.age + " 성적 : " + student2.grade);
+	}
+}
