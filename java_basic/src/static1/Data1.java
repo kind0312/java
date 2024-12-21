c7912e7f48cd04729dfcaf4bf6d83ca4e18f855c
kind0312 <kind1207@naver.com>
2024-12-21 16:21:14 +0900

    static 변수1

---
+++ java_basic/src/static1/Data1.java
@@ -0,0 +1,12 @@
+package static1;
+
+public class Data1 {
+	public String name; //예제를 단순하게 하기 위해 public으로
+	public int count;
+	
+	public Data1(String name) {
+		this.name = name;
+		count++;
+	}
+	
+}
