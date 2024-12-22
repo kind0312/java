6c96ccb28497691b58c19530a03fdd358e68e623
kind0312 <kind1207@naver.com>
2024-12-22 22:34:37 +0900

    static 메서드3

--- java_basic/src/static2/DecoDataMain2.java
+++ java_basic/src/static2/DecoDataMain2.java
@@ -1,10 +1,13 @@
 package static2;
 
-public class DecoDataMain {
+//import static static2.DecoData.staticCall;
+import static static2.DecoData.*;//모든 static 메서드를 사용하겠다.
+
+public class DecoDataMain2 {
 
 	public static void main(String[] args) {
 		System.out.println("1. 정적 호출");
-		DecoData.staticCall();
+		staticCall();
 		
 		System.out.println("2. 인스턴스 호출1");
 		DecoData data1 = new DecoData();
@@ -14,7 +17,7 @@ public class DecoDataMain {
 		DecoData data2 = new DecoData();
 		data2.instanceCall();
 		
-		DecoData.staticCall1(data1);
+		staticCall1(data1);
 		
 		//추가
 		//인스턴스를 통한 접근 //->이렇게도 가능하지만 이렇게 사용하면 클래스(static) 소속인지, 인스턴스 소속인지 명확하지 않아서 권장하지 않는다.
@@ -22,6 +25,6 @@ public class DecoDataMain {
 		data3.staticCall();
 		
 		//클래스를 통한 접근 //->static이라서 클래스를 통해 바로 접근하는걸 알 수 있다.
-		DecoData.staticCall();
+		staticCall();
 	}
 }
