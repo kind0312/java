46eae220ac88809b785a914522dfd81d56c87878
kind0312 <kind1207@naver.com>
2024-12-22 21:48:58 +0900

    static 변수3

--- java_basic/src/static1/Data3.java
+++ java_basic/src/static1/Data3.java
@@ -1,11 +1,12 @@
 package static1;
 
 public class Data3 {
-	public String name;
-	public static int count;	//static
+	public String name;						//인스턴스 변수(static이 붙지 않은 멤버 변수)
+	public static int count;	//static	// 클래스 변수(static이 붙은 멤버 변수) -> 클래스변수, 정적변수, static변수라고 부른다.
 	
 	public Data3(String name) {
 		this.name = name;
+//		Data3.count++; //-> 이렇게 안해도 된다. 내 클래스 안에 있는 변수이기때문에 아래처럼 Data3을 생략해도 된다.
 		count++;
 	}
 	
--- java_basic/src/static1/DataCountMain3.java
+++ java_basic/src/static1/DataCountMain3.java
@@ -11,5 +11,13 @@ public class DataCountMain3 {
 		
 		Data3 data3 = new Data3("C");
 		System.out.println("C count = " + Data3.count);
+		
+		//추가
+		//인스턴스를 통한 접근 --> 권장하지 않음(코드를 읽을때 이게 인스턴스 변수인가로 착각할 수 있기 때문에)
+		Data3 data4 = new Data3("D");
+		System.out.println("D count = " + data4.count); //-> 이 경우 인스턴스(힙 영역)에서 사용하는게 아니고 static이므로 메서드 영역에서 사용
+		
+		//클래스를 통한 접근
+		System.out.println("D count = " + Data3.count);
 	}
 }
